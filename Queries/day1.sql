c30d597ce3e9248ee694e841a49b2e0f20cffdf0
babak <babak.nikahval.bn@gmail.com>
2022-04-04 12:57:26 -0400

    Initial commit

---
+++ Queries/day1.sql
@@ -0,0 +1,25 @@
+select * FROM REGIONS ;
+
+-- single line comment is like this
+/*
+ multi line comment like this
+ */
+
+-- each query you write known as sql SELECT statement
+SELECT * FROM COUNTRIES;
+
+SELECT * From DEPARTMENTS;
+
+SELECT * FROM EMPLOYEES;
+
+SELECT * FROM JOBS;
+
+SELECT * FROM JOB_HISTORY;
+
+SELECT * FROM LOCATIONS;
+
+
+SELECT FIRST_NAME , LAST_NAME, EMAIL
+FROM EMPLOYEES;
+
+
